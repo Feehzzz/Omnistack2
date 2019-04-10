4dcb37b637c376ffaf5c74256cb6099fff42a6d8
Feehzzz <fehshuffle@gmail.com>
2019-04-10 15:50:09 -0300

    configurando multer

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.send('ok')
-})
+const BoxController = require('./controller/BoxController');
+const FileController = require('./controller/FileController');
+
+
+
+routes.post('/boxes', BoxController.store);
+routes.get('/boxes/:id', BoxController.show);
+routes.post('/boxes/:id/files', multer(multerConfig).single('file'), FileController.store);
+
+
+
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+
+io.on('connection', socket => {
+    console.log('ok');
+});
+
+mongoose.connect('mongodb+srv://feehmdb:feehmdb@cluster0-rctbj.mongodb.net/omnidropweek?retryWrites=true', {
+    useNewUrlParser: true
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extend: true }));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 
 app.use(require('./routes'));
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('ok')
 });
